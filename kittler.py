c0aa4cc722dad3017fafbd7ae5572cf58ee51a1e
Javier Peralta <francisco.peralta@cimat.mx>
2019-05-17 06:34:23 -0500

    Beta-VAE

---
+++ kittler.py
@@ -0,0 +1,38 @@
+from skimage import img_as_ubyte
+import numpy as np
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def Kittler(im, n_bit=8):
+    """
+    The reimplementation of Kittler-Illingworth Thresholding algorithm by Bob Pepin
+    Works on 8-bit images only
+    Original Matlab code: https://www.mathworks.com/matlabcentral/fileexchange/45685-kittler-illingworth-thresholding
+    Paper: Kittler, J. & Illingworth, J. Minimum error thresholding. Pattern Recognit. 19, 41–47 (1986).
+    """
+    m_v = 2**n_bit
+    h,g = np.histogram(im.ravel(),m_v,[0,m_v])
+    h = h.astype(np.float)
+    g = g.astype(np.float)
+    g = g[:-1]
+    c = np.cumsum(h)
+    m = np.cumsum(h * g)
+    s = np.cumsum(h * g**2)
+    sigma_f = np.sqrt(s/c - (m/c)**2)
+    cb = c[-1] - c
+    mb = m[-1] - m
+    sb = s[-1] - s
+    sigma_b = np.sqrt(sb/cb - (mb/cb)**2)
+    p =  c / c[-1]
+    v = p * np.log(sigma_f) + (1-p)*np.log(sigma_b) - p*np.log(p) - (1-p)*np.log(1-p)
+    v[~np.isfinite(v)] = np.inf
+    idx = np.argmin(v)
+    t = g[idx]
+    out = np.zeros_like(im)
+    out[im >= t] = m_v - 1
+    return out
+
+def kittler_float(im, copy=True):
+  im_k = Kittler(img_as_ubyte(im)) > 0
+  return np.array(im, copy=copy) * im_k
