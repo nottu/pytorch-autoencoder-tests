653f32f138a073feb8ca85d1a6f1b1d582b962de
Javier Peralta <francisco.peralta@cimat.mx>
2019-06-17 12:27:26 -0500

    refactor model/trainer code

--- rg_dataset.py
+++ rg_dataset.py
@@ -31,13 +31,26 @@ class LRG(data.Dataset):
 
     def __load_data(self):
         lrg = read_fwf(self.catalog_dir, skiprows=41, header=None)
-        labeled = DataFrame({'Name':lrg[0], 'Label':lrg[7]})
+        l_idx = 7 if(self.file_dir=='lrg') else 6
+        labeled = DataFrame({'Name':lrg[0], 'Label':lrg[6]})
         names = labeled['Name'].tolist()
-        labels = labeled['Label'].tolist()
+        labels_v = labeled['Label'].tolist()
         images = []
         directory, ext = self.file_dir, self.file_ext
 
+        labels = []
         for i in range(len(names)):
+          l = labels_v[i]
+          if l == 'F' or l == '1F' : l = 1
+
+          try:
+            l = float(l)
+          except Exception as the_exception:
+            continue
+
+          if(math.isnan( l )): continue
+          labels.append(int(l))
+
           f_name = '{0}/{1}.{2}'.format(directory, names[i].replace('.','_'), ext)
           im = readImg(f_name, normalize=True, sz=128)
           if self.use_kittler : im = kittler_float(im, copy=False)
@@ -46,7 +59,6 @@ class LRG(data.Dataset):
           sys.stdout.flush()
         print('')
         images = np.array(images)
-        labels = [ 1 if (l == '1' or l == '1F') else int(l) for l in labels]
 
         self.data = images
         self.labels = labels
@@ -71,116 +83,35 @@ class LRG(data.Dataset):
     def __len__(self):
         return self.data_len * self.n_aug
 
-class UNLRG_C(data.Dataset):
-    def __init__(self, sz=64, use_kittler=False, n_aug=10, catalog_dir='catalog/mrt-table4.txt', file_dir='unlrg', file_ext='fits'):
-        self.catalog_dir = catalog_dir
-        self.file_dir = file_dir
-        self.file_ext = file_ext
-        self.use_kittler = use_kittler
-        self.n_aug = n_aug
-        self.__load_data()
-        self.data_len = len(self.data)
-        self.transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.RandomRotation(180),
-            transforms.CenterCrop(80),
-            transforms.Resize(sz),
-            transforms.ToTensor()])
+class BasicDataset(data.Dataset):
+  def __init__(self, images, labels, sz=64, n_aug=10):
+    self.sz = sz
+    self.n_aug = n_aug
+    self.data = images
+    self.labels = labels
+    self.data_len = len(self.data)
+    self.transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(16),
+        # transforms.CenterCrop(80),
+        transforms.Resize(sz),
+        transforms.ToTensor()])
 
-    def __load_data(self):
-        lrg = read_fwf(self.catalog_dir, skiprows=41, header=None)
-        labeled = DataFrame({'Name':lrg[0], 'Label':lrg[8], 'Cert':lrg[11]})
-        labeled[labeled['Cert']==1]
-        names = labeled['Name'].tolist()
-        labels_v = labeled['Label'].tolist()
-        images = []
-        directory, ext = self.file_dir, self.file_ext
+  def get_data(self):
+    return self.data, self.labels
 
-        labels = []
-        for i in range(len(names)):
-          l = labels_v[i]
-          if l == 'F' : l = 1
-          l = float(l)
-          if(math.isnan( l )): continue
-          labels.append(int(l))
-          f_name = '{0}/{1}.{2}'.format(directory, names[i].replace('.','_'), ext)
-          im = readImg(f_name, normalize=True, sz=128)
-          if self.use_kittler : im = kittler_float(im, copy=False)
-          images.append(im.T)
-          sys.stdout.write('LRG:\t{}/{}\r'.format(i + 1, len(names)))
-          sys.stdout.flush()
-        print('')
-        images = np.array(images)
-        # labels = [ 1 if (l == '1' or l == 'F') else int(l) for l in labels]
+  def __getitem__(self, index):
+      index = index % self.data_len
+      np_arr = self.data[index, :]
+      y = self.labels[index]
+      ## reshape np_arr to 28x28
+      np_arr = np_arr.reshape(64, 64)
 
-        self.data = images
-        self.labels = labels
+      ## convert to PIL-image
+      img = Image.fromarray((np_arr*255).astype('uint8'))
 
-    def get_data(self):
-      return self.data, self.labels
+      return self.transform(img), y
 
-    def __getitem__(self, index):
-        index = index % self.data_len
-        np_arr = self.data[index, :]
-        y = self.labels[index]
-        ## reshape np_arr to 28x28
-        np_arr = np_arr.reshape(128, 128)
-
-        ## convert to PIL-image
-        img = Image.fromarray((np_arr*255).astype('uint8'))
-
-        #apply the transformations and return tensors
-        return self.transform(img), y
-    def __len__(self):
-        return self.data_len * self.n_aug
-
-
-class UNLRG(data.Dataset):
-    def __init__(self, sz=64, use_kittler=False, n_aug=10, catalog_dir='catalog/mrt-table4.txt', file_dir='unlrg', file_ext='fits'):
-        self.catalog_dir = catalog_dir
-        self.file_dir = file_dir
-        self.file_ext = file_ext
-        self.use_kittler = use_kittler
-        self.n_aug = n_aug
-        self.__load_data()
-        self.data_len = len(self.data)
-        self.sz = sz
-        self.transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.RandomRotation(180),
-            transforms.CenterCrop(80),
-            transforms.Resize(self.sz),
-            transforms.ToTensor()])
-
-    def __load_data(self):
-        directory, ext = self.file_dir, self.file_ext
-        names = glob.glob('{0}/*.{1}*'.format(directory, ext))
-        images = []
-        for n in range(len(names)):
-            im = readImg(names[n], normalize=True, sz=128)
-            if self.use_kittler : im = kittler_float(im, copy=False)
-            images.append( np.expand_dims(im, axis=0) )
-            sys.stdout.write('UNLRG:\t{}/{}\r'.format(n + 1, len(names)))
-            sys.stdout.flush()
-        print('')
-        self.data = np.array(images)
-            
-    def __getitem__(self, index):
-        index = index % self.data_len
-        np_arr = self.data[index, :]
-        ## reshape np_arr to 28x28
-        np_arr = np_arr.reshape(128, 128)
-
-        ## convert to PIL-image
-        img = Image.fromarray((np_arr*255).astype('uint8'))
-
-        #apply the transformations and return tensors
-        return self.transform(img)
-
-    def __len__(self):
-        return self.data_len * self.n_aug
-
-    def __repr__(self) -> str:
-        return 'unLRG dataset'
+  def __len__(self):
+      return self.data_len * self.n_aug
